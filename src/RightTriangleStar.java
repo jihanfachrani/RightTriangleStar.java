cad04718e185a0299d8bb884e55fb6c9016fe0d0
jihanfachrani <jihan.fachrani@unida.ac.id>
2022-01-22 10:12:49 +0700

    Right Triangle Star Pattern
    membuat  program java segitiga siku-siku bintang

---
+++ src/RightTriangleStar.java
@@ -0,0 +1,19 @@
+
+public class RightTriangleStar {
+
+	public static void main(String[] args) {
+		int x,y;
+        for (x = 0; x <=10;x++) { 
+          
+            System.out.println();
+            
+  
+            for (y = 0; y < x; y++) {
+               
+                System.out.print(" * ");
+              
+            }
+        }
+    }  
+}	
+	
